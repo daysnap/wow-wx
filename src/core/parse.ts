8cc1c4122efc09e800bbc2bed5a6aa468bd6fe9b
chenzhigang <979703986@qq.com>
2022-04-04 23:07:33 +0800

    subcode

--- src/core/parse.ts
+++ src/core/parse.ts
@@ -1,6 +1,8 @@
 
 import type { AppOptions, PageOptions, IAnyOne, Options } from '../interface'
 
+import { FUNCTION_PAGE_HOOKS } from '../utils/constant'
+
 const initTarget = (keys: string[]) => {
   return keys.reduce<IAnyOne>((res, key) => {
     res[key] = []
@@ -15,8 +17,14 @@ const mixinTarget = (target: IAnyOne, options: Options, keys: string[]) => {
   return target
 }
 
-const emitTarget = () => {
-  
+const nestTarget = (target: IAnyOne, options: Options) => {
+  Object.keys(target)
+    .filter(k => !!target[k].length)
+    .forEach(k => {
+      options[k] = function (...args: any) {
+        target[k].forEach((fn: any) => fn.bind(this)(...args))
+      }
+    })
 }
 
 export const parseAppOptions = (options: AppOptions) => {
@@ -32,5 +40,17 @@ export const parsePageOptions = (options: PageOptions) => {
   if (!mixins || !mixins.length) {
     return options
   }
+  const target = initTarget(FUNCTION_PAGE_HOOKS)
+
+  const mixinData: IAnyOne = {}
+  const mixinOption: IAnyOne = {}
+
+  let loop
+
+
 }
 
+const parseOptions = (options: Options) => {
+  const { mixins } = options
+  
+}
